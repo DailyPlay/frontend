315fc491520e0e6b279e80c0c3a90d31f1620160
Kopite22 <nulee1000@gmail.com>
2024-06-24 09:59:55 +0900

    [Feat] 날씨 아이콘 표시

--- src/screens/TemplateScreen/TemplateScreen.tsx
+++ src/screens/TemplateScreen/TemplateScreen.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import * as s from './style';
 import TextComponent from '@/components/Text';
 import Button from '@/components/Button';
+import WeatherIcons from '@/assets/icons/weather';
 
 const Template = () => {
   return (
@@ -10,6 +11,14 @@ const Template = () => {
       <TextComponent fontType="bodyLarge">테스팅</TextComponent>
       <Button.Default label="label" onPress={() => console.log('test')} />
       <Button.Gradient label="label" onPress={() => console.log('test')} />
+      <s.IconsWrapper>
+        <WeatherIcons.Sunny />
+        <WeatherIcons.Cloudiness />
+        <WeatherIcons.Cloudy />
+        <WeatherIcons.Rain />
+        <WeatherIcons.Snow />
+        <WeatherIcons.Thunder />
+      </s.IconsWrapper>
     </s.Container>
   );
 };
--- src/screens/TemplateScreen/style.ts
+++ src/screens/TemplateScreen/style.ts
@@ -4,3 +4,9 @@ export const Container = styled.View`
   border: 1px solid ${({theme}) => theme.color.black};
   padding: 10px;
 `;
+
+export const IconsWrapper = styled.View`
+  gap: 10px;
+  flex-direction: row;
+  margin-top: 10px;
+`;
