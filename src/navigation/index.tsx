3661ab6e89eef50b89613b52521880dfb01f804b
young il Oh <youngiloh@youngui-MacBookPro.local>
2024-06-30 10:25:59 +0900

    add Splash Screen

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import TemplateScreen from '@screens/TemplateScreen';
+import SplashScreen from 'react-native-splash-screen';
 
 const Stack = createStackNavigator();
 
 const Navigation = () => {
+  // splash screen test
+
+  useEffect(() => {
+    const splashTimer = setTimeout(() => {
+      SplashScreen.hide();
+    }, 500);
+
+    return () => clearTimeout(splashTimer);
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
