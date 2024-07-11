7b39a28480cd1c96d0ac1a550221c9d3d78fc6ed
young il Oh <youngiloh@youngui-MacBookPro.local>
2024-07-11 22:59:08 +0900

    Modal/index.tsx, constants/index.ts, modal.ts hooks/useModal.ts 추가

--- src/screens/TemplateScreen/TemplateScreen.tsx
+++ src/screens/TemplateScreen/TemplateScreen.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import * as s from './style';
 import TextComponent from '@/components/Text';
 import Button from '@/components/Button';
 import WeatherIcons from '@/assets/icons/weather';
+import ModalComponent from '@/components/Modal';
+import useModal from '@/hooks/useModal';
+import {Pressable, Text} from 'react-native';
+import {networkErrorMessage} from '@/constants';
 
 const Template = () => {
+  const {isVisible, hide, show} = useModal();
+
+  // useEffect(() => {
+  //   show();
+  // }, []);
+
   return (
     <s.Container>
       <TextComponent fontType="bodyLarge">테스팅</TextComponent>
@@ -19,6 +29,26 @@ const Template = () => {
         <WeatherIcons.Snow />
         <WeatherIcons.Thunder />
       </s.IconsWrapper>
+      <Pressable onPress={show}>
+        <Text>모달!!</Text>
+      </Pressable>
+      <ModalComponent
+        isVisible={isVisible}
+        hide={hide}
+        title={networkErrorMessage.TITLE}
+        messages={networkErrorMessage.CONTENTS}
+        buttons={[
+          {
+            label: networkErrorMessage.BUTTONS.LABEL,
+            onPress: () => console.log('재연결'),
+          },
+          // {
+          //   label: networkErrorMessage.BUTTONS.LABEL,
+          //   onPress: () => console.log('재연결'),
+          //   highlight: false,
+          // },
+        ]}
+      />
     </s.Container>
   );
 };
